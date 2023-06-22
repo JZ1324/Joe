17e34944131e20cc3c15de2bc9e0ca43c451a953
JZ1324 <91810297+JZ1324@users.noreply.github.com>
2023-06-22 21:10:34 +1000

    Update findex.js

--- findex.js
+++ findex.js
@@ -1,37 +1,20 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    // Create a scene
-    var scene = new THREE.Scene();
-
-    // Create a camera
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-
-    // Create a renderer
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    // Create a globe geometry
-    var geometry = new THREE.SphereGeometry(1, 32, 32);
-
-    // Create a material with a texture
-    var texture = new THREE.TextureLoader().load('https://jz1324.github.io/Earth/earth_texture.jpg');
-    var material = new THREE.MeshBasicMaterial({ map: texture });
-
-    // Create a mesh with the geometry and material
-    var globe = new THREE.Mesh(geometry, material);
-    scene.add(globe);
+    // Create a scene, camera, renderer, and geometry as before
+    
+    // ...
 
     // Create a variable to store the mouse coordinates
     var mouse = { x: 0, y: 0 };
 
-    // Create a variable to store the previous mouse position
-    var prevMouse = { x: 0, y: 0 };
-
     // Add event listeners for mouse movements
     document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mouseup', handleMouseUp);
+
+    // Prevent scrolling on the website
+    document.body.style.overflow = 'hidden';
 
     function handleMouseMove(event) {
         // Update the mouse coordinates based on the mouse movement
@@ -39,42 +22,34 @@ function init() {
         mouse.y = event.clientY;
     }
 
-    // Create a variable to store the rotation speed
-    var rotationSpeed = 0.005;
+    function handleMouseDown(event) {
+        // Prevent default browser behavior to avoid text selection
+        event.preventDefault();
 
-    // Create a variable to store the idle rotation
-    var idleRotation = { x: 0, y: 0 };
-
-    // Create an animation loop to continuously render the scene
-    function animate() {
-        requestAnimationFrame(animate);
-
-        // Calculate the distance moved by the mouse
-        var deltaX = mouse.x - prevMouse.x;
-        var deltaY = mouse.y - prevMouse.y;
-
-        // Update the globe's rotation based on the mouse movement
-        globe.rotation.y += deltaX * rotationSpeed;
-        globe.rotation.x += deltaY * rotationSpeed;
-
-        // Update the previous mouse position
-        prevMouse.x = mouse.x;
-        prevMouse.y = mouse.y;
-
-        // If the mouse is not moving, apply idle rotation
-        if (deltaX === 0 && deltaY === 0) {
-            globe.rotation.y += idleRotation.x;
-            globe.rotation.x += idleRotation.y;
-        } else {
-            // Store the idle rotation based on the last mouse movement
-            idleRotation.x = deltaX * rotationSpeed;
-            idleRotation.y = deltaY * rotationSpeed;
-        }
-
-        // Render the scene
-        renderer.render(scene, camera);
+        // Add event listener for mouse movement while dragging
+        document.addEventListener('mousemove', handleDrag);
     }
 
-    // Start the animation loop
-    animate();
+    function handleMouseUp(event) {
+        // Remove the event listener for mouse movement while dragging
+        document.removeEventListener('mousemove', handleDrag);
+    }
+
+    function handleDrag(event) {
+        // Calculate the distance moved by the mouse
+        var deltaX = event.clientX - mouse.x;
+        var deltaY = event.clientY - mouse.y;
+
+        // Update the globe's rotation based on the mouse movement
+        globe.rotation.y += deltaX * 0.01;
+        globe.rotation.x += deltaY * 0.01;
+
+        // Update the mouse coordinates
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+    }
+
+    // Render the scene as before
+
+    // ...
 }
