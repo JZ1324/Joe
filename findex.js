4b96c1ccf5aa45449c3abc373a33309a109fcf3c
JZ1324 <91810297+JZ1324@users.noreply.github.com>
2023-06-22 20:59:53 +1000

    Update findex.js

--- findex.js
+++ findex.js
@@ -1,17 +1,37 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    // Create a scene, camera, renderer, and geometry as before
-    
-    // ...
+    // Create a scene
+    var scene = new THREE.Scene();
+
+    // Create a camera
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    // Create a renderer
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // Create a globe geometry
+    var geometry = new THREE.SphereGeometry(1, 32, 32);
+
+    // Create a material with a texture
+    var texture = new THREE.TextureLoader().load('earth_texture.jpg');
+    var material = new THREE.MeshBasicMaterial({ map: texture });
+
+    // Create a mesh with the geometry and material
+    var globe = new THREE.Mesh(geometry, material);
+    scene.add(globe);
 
     // Create a variable to store the mouse coordinates
     var mouse = { x: 0, y: 0 };
 
+    // Create a variable to store the previous mouse position
+    var prevMouse = { x: 0, y: 0 };
+
     // Add event listeners for mouse movements
     document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mousedown', handleMouseDown);
-    document.addEventListener('mouseup', handleMouseUp);
 
     function handleMouseMove(event) {
         // Update the mouse coordinates based on the mouse movement
@@ -19,34 +39,42 @@ function init() {
         mouse.y = event.clientY;
     }
 
-    function handleMouseDown(event) {
-        // Prevent default browser behavior to avoid text selection
-        event.preventDefault();
+    // Create a variable to store the rotation speed
+    var rotationSpeed = 0.005;
 
-        // Add event listener for mouse movement while dragging
-        document.addEventListener('mousemove', handleDrag);
-    }
+    // Create a variable to store the idle rotation
+    var idleRotation = { x: 0, y: 0 };
 
-    function handleMouseUp(event) {
-        // Remove the event listener for mouse movement while dragging
-        document.removeEventListener('mousemove', handleDrag);
-    }
+    // Create an animation loop to continuously render the scene
+    function animate() {
+        requestAnimationFrame(animate);
 
-    function handleDrag(event) {
         // Calculate the distance moved by the mouse
-        var deltaX = event.clientX - mouse.x;
-        var deltaY = event.clientY - mouse.y;
+        var deltaX = mouse.x - prevMouse.x;
+        var deltaY = mouse.y - prevMouse.y;
 
         // Update the globe's rotation based on the mouse movement
-        globe.rotation.y += deltaX * 0.01;
-        globe.rotation.x += deltaY * 0.01;
+        globe.rotation.y += deltaX * rotationSpeed;
+        globe.rotation.x += deltaY * rotationSpeed;
 
-        // Update the mouse coordinates
-        mouse.x = event.clientX;
-        mouse.y = event.clientY;
+        // Update the previous mouse position
+        prevMouse.x = mouse.x;
+        prevMouse.y = mouse.y;
+
+        // If the mouse is not moving, apply idle rotation
+        if (deltaX === 0 && deltaY === 0) {
+            globe.rotation.y += idleRotation.x;
+            globe.rotation.x += idleRotation.y;
+        } else {
+            // Store the idle rotation based on the last mouse movement
+            idleRotation.x = deltaX * rotationSpeed;
+            idleRotation.y = deltaY * rotationSpeed;
+        }
+
+        // Render the scene
+        renderer.render(scene, camera);
     }
 
-    // Render the scene as before
-
-    // ...
+    // Start the animation loop
+    animate();
 }
